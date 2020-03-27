85abe996eac9c33eea593680de18cdb2cf61af06
Mathias Ferniss <5211966@RTS-365.dk>
2020-03-27 15:50:03 +0100

    er igang med få alt ove ri javascript

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -1,3 +1,22 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  var h = document.querySelector('#health');
+  var ha = document.querySelector('.healthAnimation');
+  var s = document.querySelector('#sport');
+  var sa = document.querySelector('.sportAnimation');
+  var t = document.querySelector('#travel');
+  var ta = document.querySelector('.travelAnimation');
+  h.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>HEALTH</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  h.addEventListener("click", function () {
+    alert("healt"); // ha.classList.add('clicked')
+  });
+  s.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>SPORT</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  s.addEventListener("click", function () {
+    alert("SPORT"); // ha.classList.add('clicked')
+  });
+  t.innerHTML = "\n\n<figur class=\"flex items-center\">\n            <img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n            <h1>TRAVEL</h1>\n        </figur>\n        <figure class=\"pr-10\">\n            <img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n        </figure>\n";
+  t.addEventListener("click", function () {
+    alert("travel"); // ha.classList.add('clicked')
+  });
+});
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,52 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+let h = document.querySelector('#health')
+let ha = document.querySelector('.healthAnimation')
+let s = document.querySelector('#sport')
+let sa = document.querySelector('.sportAnimation')
+let t = document.querySelector('#travel')
+let ta = document.querySelector('.travelAnimation')
 
+h.innerHTML = `
+<figure class="flex items-center">
+<img src="/assets/images/tema.png" alt="" class="w-32 h-32">
+<h1>HEALTH</h1>
+</figure>
+<figure class="pr-10">
+<img class="healthAnimation" src="/assets/images/Arow.png" alt="">
+</figure>
+`
+h.addEventListener("click", () =>{
+    alert("healt")
+   // ha.classList.add('clicked')
+})
 
+s.innerHTML = `
+<figure class="flex items-center">
+<img src="/assets/images/tema.png" alt="" class="w-32 h-32">
+<h1>SPORT</h1>
+</figure>
+<figure class="pr-10">
+<img class="healthAnimation" src="/assets/images/Arow.png" alt="">
+</figure>
+`
+s.addEventListener("click", () =>{
+    alert("SPORT")
+   // ha.classList.add('clicked')
+})
 
+t.innerHTML = `
 
+<figur class="flex items-center">
+            <img src="/assets/images/tema.png" alt="" class="w-32 h-32">
+            <h1>TRAVEL</h1>
+        </figur>
+        <figure class="pr-10">
+            <img class="healthAnimation" src="/assets/images/Arow.png" alt="">
+        </figure>
+`
+t.addEventListener("click", () =>{
+    alert("travel")
+   // ha.classList.add('clicked')
+})
 
 })
