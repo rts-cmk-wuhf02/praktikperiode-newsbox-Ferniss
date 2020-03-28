8858b8d88111c59eae8472d89bf47cac5b43054c
Mathias Ferniss <5211966@RTS-365.dk>
2020-03-28 13:09:55 +0100

    har fået lavet så der kommer tekst frem

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -2,21 +2,28 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   var h = document.querySelector('#health');
-  var ha = document.querySelector('.healthAnimation');
-  var s = document.querySelector('#sport');
-  var sa = document.querySelector('.sportAnimation');
-  var t = document.querySelector('#travel');
-  var ta = document.querySelector('.travelAnimation');
   h.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>HEALTH</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  var ha = document.querySelector('.healthAnimation');
   h.addEventListener("click", function () {
-    alert("healt"); // ha.classList.add('clicked')
+    ha.classList.toggle('clicked');
+    var sh = document.querySelector('#sectionhealth');
+
+    if (ha.className === "healthAnimation clicked") {
+      sh.innerHTML = "\n          <section class=\"flex justify-between items-center pt-2 my-3 p-6\">\n            <figur class=\"flex items-center justify-center\">\n                <img src=\"/assets/images/undertema.png\" alt=\"\" class=\"rounded-full h-16 w-16\">\n                <section>\n                    <h1>Headline</h1>\n    \n                    <p>\n                        Surfing is a surface water sport in which the wave rider, referred to as...\n                    </p>\n                </section>\n            </figur>\n        </section>\n          ";
+    } else {
+      sh.innerHTML = "";
+    }
   });
-  s.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>SPORT</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  var s = document.querySelector('#sport');
+  s.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>SPORT</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"sportAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  var sa = document.querySelector('.sportAnimation');
   s.addEventListener("click", function () {
-    alert("SPORT"); // ha.classList.add('clicked')
+    sa.classList.toggle('clicked');
   });
-  t.innerHTML = "\n\n<figur class=\"flex items-center\">\n            <img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n            <h1>TRAVEL</h1>\n        </figur>\n        <figure class=\"pr-10\">\n            <img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n        </figure>\n";
+  var t = document.querySelector('#travel');
+  t.innerHTML = "\n<figur class=\"flex items-center\">\n            <img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n            <h1>TRAVEL</h1>\n        </figur>\n        <figure class=\"pr-10\">\n            <img class=\"travelAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n        </figure>\n";
+  var ta = document.querySelector('.travelAnimation');
   t.addEventListener("click", function () {
-    alert("travel"); // ha.classList.add('clicked')
+    ta.classList.toggle('clicked');
   });
 });
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 let h = document.querySelector('#health')
-let ha = document.querySelector('.healthAnimation')
-let s = document.querySelector('#sport')
-let sa = document.querySelector('.sportAnimation')
-let t = document.querySelector('#travel')
-let ta = document.querySelector('.travelAnimation')
-
 h.innerHTML = `
 <figure class="flex items-center">
 <img src="/assets/images/tema.png" alt="" class="w-32 h-32">
@@ -15,38 +9,59 @@ h.innerHTML = `
 <img class="healthAnimation" src="/assets/images/Arow.png" alt="">
 </figure>
 `
-h.addEventListener("click", () =>{
-    alert("healt")
-   // ha.classList.add('clicked')
-})
+let ha = document.querySelector('.healthAnimation')
+    h.addEventListener("click", () =>{
+        ha.classList.toggle('clicked')
+        let sh = document.querySelector('#sectionhealth')
+        if (ha.className === "healthAnimation clicked") {
+          sh.innerHTML = `
+          <section class="flex justify-between items-center pt-2 my-3 p-6">
+            <figur class="flex items-center justify-center">
+                <img src="/assets/images/undertema.png" alt="" class="rounded-full h-16 w-16">
+                <section>
+                    <h1>Headline</h1>
+    
+                    <p>
+                        Surfing is a surface water sport in which the wave rider, referred to as...
+                    </p>
+                </section>
+            </figur>
+        </section>
+          `;
+        } else {
+            sh.innerHTML = "";
+        }
+    })
+    
 
+let s = document.querySelector('#sport')
 s.innerHTML = `
 <figure class="flex items-center">
 <img src="/assets/images/tema.png" alt="" class="w-32 h-32">
 <h1>SPORT</h1>
 </figure>
 <figure class="pr-10">
-<img class="healthAnimation" src="/assets/images/Arow.png" alt="">
+<img class="sportAnimation" src="/assets/images/Arow.png" alt="">
 </figure>
 `
-s.addEventListener("click", () =>{
-    alert("SPORT")
-   // ha.classList.add('clicked')
-})
 
+let sa = document.querySelector('.sportAnimation')
+    s.addEventListener("click", () =>{
+        sa.classList.toggle('clicked')
+    })
+
+let t = document.querySelector('#travel')
 t.innerHTML = `
-
 <figur class="flex items-center">
             <img src="/assets/images/tema.png" alt="" class="w-32 h-32">
             <h1>TRAVEL</h1>
         </figur>
         <figure class="pr-10">
-            <img class="healthAnimation" src="/assets/images/Arow.png" alt="">
+            <img class="travelAnimation" src="/assets/images/Arow.png" alt="">
         </figure>
 `
+let ta = document.querySelector('.travelAnimation')
 t.addEventListener("click", () =>{
-    alert("travel")
-   // ha.classList.add('clicked')
-})
-
+        ta.classList.toggle('clicked')
+    })
 })
