c51fc0fa6207337522d1d722ab665dc4761890a6
Mathias Ferniss <5211966@RTS-365.dk>
2020-03-30 08:08:10 +0200

    the newsbox is working just without the api. need to add the settin site and connect them and add all the functions

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -1,6 +1,19 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
+  var e = document.querySelector('#europe');
+  e.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>EUROPE</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"europeAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
+  var ea = document.querySelector('.europeAnimation');
+  e.addEventListener("click", function () {
+    ea.classList.toggle('clicked');
+    var se = document.querySelector('#sectioneurope');
+
+    if (ea.className === "europeAnimation clicked") {
+      se.innerHTML = "\n          <section class=\"flex justify-between items-center pt-2 my-3 p-6\">\n            <figur class=\"flex items-center justify-center\">\n                <img src=\"/assets/images/undertema.png\" alt=\"\" class=\"rounded-full h-16 w-16\">\n                <section>\n                    <h1>Headline</h1>\n    \n                    <p>\n                        Surfing is a surface water sport in which the wave rider, referred to as...\n                    </p>\n                </section>\n            </figur>\n        </section>\n          ";
+    } else {
+      se.innerHTML = "";
+    }
+  });
   var h = document.querySelector('#health');
   h.innerHTML = "\n<figure class=\"flex items-center\">\n<img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n<h1>HEALTH</h1>\n</figure>\n<figure class=\"pr-10\">\n<img class=\"healthAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n</figure>\n";
   var ha = document.querySelector('.healthAnimation');
@@ -19,11 +32,38 @@ document.addEventListener('DOMContentLoaded', function () {
   var sa = document.querySelector('.sportAnimation');
   s.addEventListener("click", function () {
     sa.classList.toggle('clicked');
+    var ss = document.querySelector('#sectionsport');
+
+    if (sa.className === "sportAnimation clicked") {
+      ss.innerHTML = "\n          <section class=\"flex justify-between items-center pt-2 my-3 p-6\">\n            <figur class=\"flex items-center justify-center\">\n                <img src=\"/assets/images/undertema.png\" alt=\"\" class=\"rounded-full h-16 w-16\">\n                <section>\n                    <h1>Headline</h1>\n    \n                    <p>\n                        Surfing is a surface water sport in which the wave rider, referred to as...\n                    </p>\n                </section>\n            </figur>\n        </section>\n          ";
+    } else {
+      ss.innerHTML = "";
+    }
+  });
+  var b = document.querySelector('#buisness');
+  b.innerHTML = "\n<figur class=\"flex items-center\">\n            <img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n            <h1>BUISNESS</h1>\n        </figur>\n        <figure class=\"pr-10\">\n            <img class=\"buisnessAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n        </figure>\n";
+  var ba = document.querySelector('.buisnessAnimation');
+  b.addEventListener("click", function () {
+    ba.classList.toggle('clicked');
+    var sb = document.querySelector('#sectionbuisness');
+
+    if (ba.className === "buisnessAnimation clicked") {
+      sb.innerHTML = "\n          <section class=\"flex justify-between items-center pt-2 my-3 p-6\">\n            <figur class=\"flex items-center justify-center\">\n                <img src=\"/assets/images/undertema.png\" alt=\"\" class=\"rounded-full h-16 w-16\">\n                <section>\n                    <h1>Headline</h1>\n    \n                    <p>\n                        Surfing is a surface water sport in which the wave rider, referred to as...\n                    </p>\n                </section>\n            </figur>\n        </section>\n          ";
+    } else {
+      sb.innerHTML = "";
+    }
   });
   var t = document.querySelector('#travel');
   t.innerHTML = "\n<figur class=\"flex items-center\">\n            <img src=\"/assets/images/tema.png\" alt=\"\" class=\"w-32 h-32\">\n            <h1>TRAVEL</h1>\n        </figur>\n        <figure class=\"pr-10\">\n            <img class=\"travelAnimation\" src=\"/assets/images/Arow.png\" alt=\"\">\n        </figure>\n";
   var ta = document.querySelector('.travelAnimation');
   t.addEventListener("click", function () {
     ta.classList.toggle('clicked');
+    var st = document.querySelector('#sectiontravel');
+
+    if (ta.className === "travelAnimation clicked") {
+      st.innerHTML = "\n          <section class=\"flex justify-between items-center pt-2 my-3 p-6\">\n            <figur class=\"flex items-center justify-center\">\n                <img src=\"/assets/images/undertema.png\" alt=\"\" class=\"rounded-full h-16 w-16\">\n                <section>\n                    <h1>Headline</h1>\n    \n                    <p>\n                        Surfing is a surface water sport in which the wave rider, referred to as...\n                    </p>\n                </section>\n            </figur>\n        </section>\n          ";
+    } else {
+      st.innerHTML = "";
+    }
   });
 });
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,44 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    let e = document.querySelector('#europe')
+e.innerHTML = `
+<figure class="flex items-center">
+<img src="/assets/images/tema.png" alt="" class="w-32 h-32">
+<h1>EUROPE</h1>
+</figure>
+<figure class="pr-10">
+<img class="europeAnimation" src="/assets/images/Arow.png" alt="">
+</figure>
+`
+let ea = document.querySelector('.europeAnimation')
+    e.addEventListener("click", () =>{
+        ea.classList.toggle('clicked')
+        let se = document.querySelector('#sectioneurope')
+        
+        if (ea.className === "europeAnimation clicked") {
+          se.innerHTML = `
+          <section class="flex justify-between items-center pt-2 my-3 p-6">
+            <figur class="flex items-center justify-center">
+                <img src="/assets/images/undertema.png" alt="" class="rounded-full h-16 w-16">
+                <section>
+                    <h1>Headline</h1>
+    
+                    <p>
+                        Surfing is a surface water sport in which the wave rider, referred to as...
+                    </p>
+                </section>
+            </figur>
+        </section>
+          `;
+        } else {
+            se.innerHTML = "";
+        }
+    })
+
+
+
+
+
+
 let h = document.querySelector('#health')
 h.innerHTML = `
 <figure class="flex items-center">
@@ -13,6 +53,7 @@ let ha = document.querySelector('.healthAnimation')
     h.addEventListener("click", () =>{
         ha.classList.toggle('clicked')
         let sh = document.querySelector('#sectionhealth')
+        
         if (ha.className === "healthAnimation clicked") {
           sh.innerHTML = `
           <section class="flex justify-between items-center pt-2 my-3 p-6">
@@ -48,6 +89,59 @@ s.innerHTML = `
 let sa = document.querySelector('.sportAnimation')
     s.addEventListener("click", () =>{
         sa.classList.toggle('clicked')
+        let ss = document.querySelector('#sectionsport')
+        if (sa.className === "sportAnimation clicked") {
+          ss.innerHTML = `
+          <section class="flex justify-between items-center pt-2 my-3 p-6">
+            <figur class="flex items-center justify-center">
+                <img src="/assets/images/undertema.png" alt="" class="rounded-full h-16 w-16">
+                <section>
+                    <h1>Headline</h1>
+    
+                    <p>
+                        Surfing is a surface water sport in which the wave rider, referred to as...
+                    </p>
+                </section>
+            </figur>
+        </section>
+          `;
+        } else {
+            ss.innerHTML = "";
+        }
+    })
+    
+let b = document.querySelector('#buisness')
+b.innerHTML = `
+<figur class="flex items-center">
+            <img src="/assets/images/tema.png" alt="" class="w-32 h-32">
+            <h1>BUISNESS</h1>
+        </figur>
+        <figure class="pr-10">
+            <img class="buisnessAnimation" src="/assets/images/Arow.png" alt="">
+        </figure>
+`
+let ba = document.querySelector('.buisnessAnimation')
+b.addEventListener("click", () =>{
+        ba.classList.toggle('clicked')
+        let sb = document.querySelector('#sectionbuisness')
+        if (ba.className === "buisnessAnimation clicked") {
+          sb.innerHTML = `
+          <section class="flex justify-between items-center pt-2 my-3 p-6">
+            <figur class="flex items-center justify-center">
+                <img src="/assets/images/undertema.png" alt="" class="rounded-full h-16 w-16">
+                <section>
+                    <h1>Headline</h1>
+    
+                    <p>
+                        Surfing is a surface water sport in which the wave rider, referred to as...
+                    </p>
+                </section>
+            </figur>
+        </section>
+          `;
+        } else {
+            sb.innerHTML = "";
+        }
     })
 
 let t = document.querySelector('#travel')
@@ -63,5 +157,24 @@ t.innerHTML = `
 let ta = document.querySelector('.travelAnimation')
 t.addEventListener("click", () =>{
         ta.classList.toggle('clicked')
+        let st = document.querySelector('#sectiontravel')
+        if (ta.className === "travelAnimation clicked") {
+          st.innerHTML = `
+          <section class="flex justify-between items-center pt-2 my-3 p-6">
+            <figur class="flex items-center justify-center">
+                <img src="/assets/images/undertema.png" alt="" class="rounded-full h-16 w-16">
+                <section>
+                    <h1>Headline</h1>
+    
+                    <p>
+                        Surfing is a surface water sport in which the wave rider, referred to as...
+                    </p>
+                </section>
+            </figur>
+        </section>
+          `;
+        } else {
+            st.innerHTML = "";
+        }
     })
 })
